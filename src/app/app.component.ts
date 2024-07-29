8ce0728d20d862eb31a714c08ab2be14b58a4e17
anapbuchholz@gmail.com <anapbuchholz@gmail.com>
2024-07-29 19:44:44 -0300

    Criando os primeiros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MeuPrimeiroComponent, MeuPrimeiro2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'thrift-store';
+  title = 'Mundo!';
 }
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro componente com Angular</p>
+    `,
+})
+export class MeuPrimeiroComponent {
+
+}
